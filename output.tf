b350e26807e052645bf04e14d5a3f3d31f2ffe11
Anthony Angelo <anthonyangelo@yahoo.com>
2023-03-20 20:47:50 -0400

    Update

--- output.tf
+++ output.tf
@@ -3,5 +3,5 @@ output "resource_group_name" {
 }
 
 output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.theseus-vm.public_ip_address
+  value = azurerm_linux_virtual_machine.telemachus-vm.public_ip_address
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 # The public SSH key to use
 variable "public_key_path" {
-  description = "Public SSH Key to use (e.g: ~/.ssh/theseus.pub)"
-  default = "~/.ssh/theseus.pub"
+  description = "Public SSH Key to use (e.g: ~/.ssh/telemachus.pub)"
+  default = "~/.ssh/telemachus.pub"
 }
 
 # Resource Group Location
